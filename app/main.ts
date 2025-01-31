92776bbe9bd3db18ad99b286660ee730f63fbc07
mcdotjs <mirectm@gmail.com>
2025-01-31 20:14:22 +0100

    type builtin

--- app/main.ts
+++ app/main.ts
@@ -1,19 +1,26 @@
 import { ReadLine, createInterface } from "readline";
-
+//🚀
 const rl = createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-
 const f = () => {
   rl.question("$ ", (answer: string) => {
     if (answer == 'exit 0') {
-      commands['exit']()
+      commands['exit'].value()
     }
 
     const args = answer.split(" ");
-    if (commands[args[0]]) {
-      commands[args[0]](rl, answer.substring(5) + '\n')
+    if (args[0] == 'type' && commands[args[1]] && commands[args[1]].isBuiltin) {
+      if (args[1] != "exit") {
+        commands[args[1]].value(rl, commands[args[1]].name + ' is a shell builtin\n')
+      } else {
+        rl.write(`exit is a shell builtin\n`)
+      }
+    } else if (args[0] == 'type' && !commands[args[1]]) {
+      rl.write(`${args[1]}: not found\n`)
+    } else if (commands[args[0]]) {
+      commands[args[0]].value(rl, answer.substring(5) + '\n')
     } else {
       rl.write(`${answer}: command not found\n`)
     }
@@ -24,11 +31,32 @@ const f = () => {
 f()
 
 const commands: Command = {
-  'echo': (rl: ReadLine, answer: string) => { rl.write(answer) },
-  "exit": () => process.exit()
+  echo: {
+    name: 'echo',
+    value: (rl: ReadLine, answer: string) => {
+      rl.write(answer)
+    },
+    isBuiltin: true
+  },
+  exit: {
+    name: "exit",
+    value: () => process.exit(),
+    isBuiltin: true
+  },
+  type: {
+    name: "type",
+    value: (rl: ReadLine, answer: string) => {
+      rl.write(answer)
+    },
+    isBuiltin: true
+  }
 }
 
 //INFO: types
 type Command = {
-  [key: string]: any
+  [key: string]: {
+    name: string,
+    value?: any
+    isBuiltin?: boolean
+  }
 }
