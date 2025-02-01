5de4e04ca9e083891c817fddd31b5ff8dca4463c
mcdotjs <mirectm@gmail.com>
2025-02-01 13:48:34 +0100

    type executable files

--- app/main.ts
+++ app/main.ts
@@ -4,25 +4,15 @@ const rl = createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+
 const f = () => {
   rl.question("$ ", (answer: string) => {
-    if (answer == 'exit 0') {
-      commands['exit'].value()
-    }
-
     const args = answer.split(" ");
-    if (args[0] == 'type' && commands[args[1]] && commands[args[1]].isBuiltin) {
-      if (args[1] != "exit") {
-        commands[args[1]].value(rl, commands[args[1]].name + ' is a shell builtin\n')
-      } else {
-        rl.write(`exit is a shell builtin\n`)
-      }
-    } else if (args[0] == 'type' && !commands[args[1]]) {
-      rl.write(`${args[1]}: not found\n`)
-    } else if (commands[args[0]]) {
-      commands[args[0]].value(rl, answer.substring(5) + '\n')
+
+    if (args[0] == 'exit' || args[1] == 'exit') {
+      handleExit(answer)
     } else {
-      rl.write(`${answer}: command not found\n`)
+      handleOtherArgs(answer)
     }
     f()
   })
@@ -50,6 +40,53 @@ const commands: Command = {
     },
     isBuiltin: true
   }
+}
+
+const handleOtherArgs = (answer: string) => {
+  const args = answer.split(" ");
+  if (args[0] == 'type' && commands[args[1]] && commands[args[1]].isBuiltin) {
+    commands[args[1]].value(rl, commands[args[1]].name + ' is a shell builtin\n')
+  } else if (args[0] == 'type' && !commands[args[1]]) {
+    //WARNING: !!!!
+    const p = process.env.PATH2 ?? ""
+    const { realRoute, isInPath } = commandIsInPath(args[1], p)
+    if (isInPath) {
+      rl.write(`${args[1]} is ${realRoute}\n`)
+    } else {
+      rl.write(`${args[1]}: not found\n`)
+    }
+  } else if (commands[args[0]]) {
+    commands[args[0]].value(rl, answer.substring(5) + '\n')
+  } else {
+    rl.write(`${answer}: command not found\n`)
+  }
+}
+
+const handleExit = (answer: string) => {
+  if (answer == 'exit 0') {
+    commands['exit'].value()
+  } else if (answer == "type exit") {
+    rl.write(`exit is a shell builtin\n`)
+  } else if (answer == "echo exit") {
+    rl.write(`exit\n`)
+  }
+}
+
+const commandIsInPath = (command: string, path: string) => {
+  const splited = path.split(':')
+  let isInPath = false
+  let realRoute = null
+  for (const s in splited) {
+    const splitedDir = splited[s].split('/')
+    for (const d in splitedDir) {
+      if (splitedDir[d] == command) {
+        realRoute = splited[s]
+        isInPath = true
+        break
+      }
+    }
+  }
+  return { isInPath, realRoute }
 }
 
 //INFO: types
